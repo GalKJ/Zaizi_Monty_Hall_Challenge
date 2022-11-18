980812080e3ed1dfe90958ef5ce28e442e50b56d
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-18 10:50:33 +0000

    add draw grid and start game functions

--- index.js
+++ index.js
@@ -6,10 +6,32 @@
         const door = document.createElement('div');
         door.className = 'door';
         door.id = `door${column}`;
-        door.textContent = letter;
+        door.textContent = prize;
 
         container.appendChild(door);
         
         return door;
     }
+
+// Function to draw grid for doors.
+    function drawGrid(container) {
+        const grid = document.createElement('div');
+        grid.className = 'grid';
+    
+        for (let i = 0; i < 3; i++) {
+                drawDoor(grid, i)
+                
+            }
+    
+        container.appendChild(grid);
+    }
+
+// Function to begin the game and draw the UI.
+    function startGame() {
+        const game = document.querySelector("#game-container");
+        drawGrid(game);
+ 
+    }
+
+    startGame();
 
