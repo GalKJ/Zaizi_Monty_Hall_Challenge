b4321bb0b8de1a4b51bc908e56ea7d1d83cd22c6
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-18 10:41:46 +0000

    add draw door function

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // Zaizi Monty Hall index.js file
 
 
+// The below function draws the doors that will contain prizes as their text content 
+    function drawDoor(container, column, prize = '') {
+        const door = document.createElement('div');
+        door.className = 'door';
+        door.id = `door${column}`;
+        door.textContent = letter;
+
+        container.appendChild(door);
+        
+        return door;
+    }
+
